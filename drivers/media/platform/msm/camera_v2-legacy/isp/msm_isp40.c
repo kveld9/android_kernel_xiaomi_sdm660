1ec4c50e8e39311cbe81f67fa07a237c981d9a34
kveld <kveld912@proton.me>
2026-09-07 01:24:15 -0300

    fix(camera_v2-legacy): support out-of-tree builds and fix clang/linux-4.19 compatibility
    Change-Id: I841d0209b8debe054215b340031f134f2a4e3e89

--- drivers/media/platform/msm/camera_v2-legacy/isp/msm_isp40.c
+++ drivers/media/platform/msm/camera_v2-legacy/isp/msm_isp40.c
@@ -234,13 +234,12 @@ static int32_t msm_vfe40_init_qos_parms(struct vfe_device *vfe_dev,
 					pr_err("%s: NO D/S settings\n",
 						__func__);
 					kfree(ds_settings);
-					kfree(ds_regs);
-	} else {
+				} else {
 					for (i = 0; i < ds_entries; i++)
 						msm_camera_io_w(ds_settings[i],
 							vfebase + ds_regs[i]);
-						kfree(ds_regs);
-						kfree(ds_settings);
+					kfree(ds_regs);
+					kfree(ds_settings);
 				}
 			} else {
 				kfree(ds_regs);
